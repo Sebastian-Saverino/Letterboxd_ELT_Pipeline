78472735a38b209e7e1b94cc3ae2ca17df2f7c48
Sebastian Saverino <sebastian.saverino4@gmail.com>
2026-02-22 01:56:28 -0800

    Create dbt folder

---
+++ dbt/letterboxd/target/run/letterboxd/models/silver/silver_ratings.sql
@@ -0,0 +1,55 @@
+
+  
+    
+
+  create  table "letterboxd_warehouse"."silver_silver"."silver_ratings__dbt_tmp"
+  
+  
+    as
+  
+  (
+    
+
+with src as (
+    select
+        nullif(trim(list_date), '') as list_date_txt,
+        nullif(trim(name), '') as name,
+        nullif(trim(year), '') as year_txt,
+        nullif(trim(letterboxd_uri), '') as letterboxd_uri,
+        nullif(trim(rating), '') as rating_txt
+    from "letterboxd_warehouse"."bronze"."ratings"
+),
+
+typed as (
+    select
+        case when list_date_txt ~ '^\d{4}-\d{2}-\d{2}$' then list_date_txt::date end as list_date,
+        name,
+        case when year_txt ~ '^\d{4}$' then year_txt::int end as year,
+        letterboxd_uri,
+        case when rating_txt ~ '^\d+(\.\d+)?$' then rating_txt::numeric(3,1) end as rating
+    from src
+),
+
+deduped as (
+    select *
+    from (
+        select
+            *,
+            row_number() over (
+                partition by letterboxd_uri, list_date, rating
+                order by letterboxd_uri
+            ) as rn
+        from typed
+    ) t
+    where rn = 1
+)
+
+select
+    list_date,
+    name,
+    year,
+    letterboxd_uri,
+    rating
+from deduped
+  );
+  
